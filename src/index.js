3f104cac951ec92abdec8e903707d0820332b737
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-07 18:23:41 +0300

    removed webpack and index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import calculator from './calculator';
-import capitalized from './capitilaze';
-import analyze from './analysis';
-import cipher from './ceaserCipher';
-import reversed from './reverse';
